102660937e9c5a742fe8d7dc0655a8505c46dc08
hooh_hooah <hooh_hooah@protonmail.com>
2021-09-23 12:39:44 +0900

    Added Studio Object Reference.

---
+++ HooahUtility/IL_HooahUI/Serialization/StudioReference/StudioObjectReference.cs
@@ -0,0 +1,14 @@
+﻿using MessagePack;
+#if AI || HS2
+#endif
+
+namespace HooahUtility.Serialization.StudioReference
+{
+    [MessagePackObject]
+    public class StudioObjectReference : StudioReference
+    {
+#if AI || HS2
+        [IgnoreMember] public string Name => Reference?.treeNodeObject?.textName ?? "Not Assigned";
+#endif
+    }
+}
