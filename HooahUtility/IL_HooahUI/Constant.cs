30f8435499c9c9e39abe0158dc406886eeac0d24
hooh-hooah <hooh-hooah@protonmail.com>
2021-10-06 23:13:24 +0900

    Raised the minor version number of HooahUtility Reaseon: fixed the duplication glitch

--- HooahUtility/IL_HooahUI/Constant.cs
+++ HooahUtility/IL_HooahUI/Constant.cs
@@ -2,5 +2,5 @@
 {
     public const string GUID = "com.hooh.hooah.utility";
     public const string NAME = "Hooah Utility";
-    public const string VERSION = "1.2.0";
+    public const string VERSION = "1.2.1";
 }
