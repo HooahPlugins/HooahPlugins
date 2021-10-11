31151ca791fd0a671e6051a3e69f6bd73b2f8b9d
hooh_hooah <hooh_hooah@protonmail.com>
2021-10-11 18:31:42 +0900

    Cleanup Git freak out

--- HooahUtility/IL_HooahUI/Utility/ObjectControlInfoUtility.cs
+++ HooahUtility/IL_HooahUI/Utility/ObjectControlInfoUtility.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: HooahUtility.Utility.ObjectControlInfoUtility
-// Assembly: HooahComponents, Version=2.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 36F76C18-36CE-4019-97EF-FF9FBC728F0D
-// Assembly location: D:\projects\HooahPlugins\HooahComponents\AI_Hooah\bin\Release\final\AI_Hooah.dll
-
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
--- HooahUtility/IL_HooahUI/Utility/StringUtility.cs
+++ HooahUtility/IL_HooahUI/Utility/StringUtility.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Utility.StringUtility
-// Assembly: HooahComponents, Version=2.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 36F76C18-36CE-4019-97EF-FF9FBC728F0D
-// Assembly location: D:\projects\HooahPlugins\HooahComponents\AI_Hooah\bin\Release\final\AI_Hooah.dll
-
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Utility
 {
