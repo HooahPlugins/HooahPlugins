bf0a242918c1205a19fcacd3dbbe4c0c97890aee
hooh_hooah <hooh_hooah@protonmail.com>
2021-11-01 21:14:05 +0900

    Added Vector3 Form Components

---
+++ HooahUtility/IL_HooahUI/Controller/Components/DragArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace HooahUtility.Controller.Components
+{
+    public class DragArea : MonoBehaviour, IDragHandler
+    {
+        public event Action<PointerEventData> onDrag;
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            onDrag?.Invoke(eventData);
+        }
+    }
+}
